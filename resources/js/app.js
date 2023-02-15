c4ca9f6f08590eec969dce5473a949c1872ea21a
Alexander Storchak <go280286sai@gmail.com>
2023-02-15 21:45:23 +0200

    add project

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+import './bootstrap';
+import './assets/jquery.min';
+import './assets/bootstrap.bundle.min';
+import './assets/owl-carousel';
+import './assets/imagesloaded';
+import './assets/templatemo-custom';
+import './assets/jquery-1.10.2';
+import './assets/jquery.metisMenu';
+import './assets/bootstrap.min';
+// import './assets/custom';
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+window.Alpine = Alpine;
+
+Alpine.plugin(focus);
+
+Alpine.start();
