e8cc1108126f8589bc281d4350819f7dad3947d9
go280286sai <go280286sai@gmail.com>
2023-05-01 16:35:41 +0300

    add react apx. Create func:getPredict

--- resources/views/admin/document/create.blade.php
+++ resources/views/admin/document/create.blade.php
@@ -66,12 +66,7 @@
                         </div>
                     </div>
                 </form>
-
-                <div class="box-footer">
-                    <input type="button" onclick="getPredict()"
-                           class="btn btn-success pull-left"
-                           value="Подобрать варианты">
-                </div>
+                <input type="hidden" id="getEmail" value="#" />
                 <div id="body"></div>
             </div>
         </section>
@@ -79,6 +74,7 @@
 @endsection
 
 @section('js')
+    @vite('resources/js/app.jsx');
     <script src="{{env('APP_URL').'/assets/plugins/datatables/jquery.dataTables.min.js'}}"></script>
     <script src="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.min.js'}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
@@ -98,57 +94,6 @@
                 radioClass: 'iradio_minimal-blue'
             });
         });
-    </script>
-    <script>function getPredict(email) {
-            let rooms = $('#rooms').val();
-            let etajnost = $('#etajnost').val();
-            let price = $('#price').val();
-            let location = $('#location').val();
-            let object = {'rooms': rooms, 'etajnost': etajnost, 'price': price, 'location': location};
-            axios.post('http://192.168.0.107:8000/getPredict', JSON.stringify(object)).then(df => {
-                axios.post('/user/getApartment', {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                    'text': df.data
-                }).then(data => {
-                    let text = `<table id="example1" class="table table-bordered table-striped">
-                        <thead>
-                        <tr class="bg-orange-400">
-                            <th scope="col">Название</th>
-                            <th scope="col">Комнаты</th>
-                            <th scope="col">Этаж</th>
-                            <th scope="col">Этажность</th>
-                            <th scope="col">Описание</th>
-                            <th scope="col">Цена</th>
-                            <th scope="col">Расположение</th>
-                        </tr>
-                        </thead>
-                        <tbody>`;
-                    let list = '';
-                    for (let index in data.data[0]) {
-                        list += data.data[0][index].id + ',';
-                        text +=
-                            `<tr>
-                            <td class="bg-orange-200"><a href="${data.data[0][index].url}" target="_blank">${data.data[0][index].title}</a></td>
-                            <td class="bg-orange-200">${data.data[0][index].rooms}</td>
-                            <td class="bg-orange-200">${data.data[0][index].floor}</td>
-                            <td class="bg-orange-200">${data.data[0][index].etajnost}</td>
-                            <td class="bg-orange-200">${data.data[0][index].description}</td>
-                            <td class="bg-orange-200">${data.data[0][index].price}</td>
-                            <td class="bg-orange-200">${data.data[0][index].location}</td>
-                            </tr>`
-                        ;
-                    }
-
-
-                    text += `</tbody>
-                    </table>
-</form>`;
-                    $('#body').html(text);
-                })
-            }).catch(err => {
-                console.log(err.messages)
-            })
-        }
     </script>
 
 @endsection
